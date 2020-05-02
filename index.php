1e9adf1a0c4afe2eae1c454e71fd7469689b6c8c
BasicCode1 <64691140+BasicCode1@users.noreply.github.com>
2020-05-02 22:12:52 +0700

    upload web calculate

---
+++ index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>homepage</title>
+
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <form action="calculate.php" method="post">
+        <div class="header">
+            Web Calculate
+        </div>
+        <div class="input-group">
+            <table>
+                <td><label for="username">username : </label></td>
+                <td><input type="text" name="username" required></td>
+                <td><input class="btn" type="submit" value="submit"></td>
+            </table>
+        </div>
+    </form>
+</body>
+
+</html>
